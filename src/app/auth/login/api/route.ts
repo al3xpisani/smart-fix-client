9b0cac323c96434215c4344f2da76b1eeb6a860b
al3xpisani <alexandre.pisani.ant@gmail.com>
2024-12-07 14:59:05 -0300

    form fields login register

--- src/app/auth/login/api/route.ts
+++ src/app/auth/login/api/route.ts
@@ -6,8 +6,10 @@ export async function POST(request: NextRequest) {
     const { email, password } = LoginValidation.parse(await request.json());
     console.log('email.... ' + email, 'password.... ' + password);
     return new NextResponse('It worked.');
-  } catch (error: any) {
-    console.log(error.message);
-    return new NextResponse(error.message, { status: 400 });
+  } catch (error) {
+    if(error instanceof Error){
+      console.log(error.message);
+      return new NextResponse(error.message, { status: 400 });
+    }
   }
 }
